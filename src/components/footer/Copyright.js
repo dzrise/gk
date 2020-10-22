4eedd9701809132774e3f0d28f7ca3f04cfb5c57
Ваше Имя <you@example.com>
2020-10-22 15:23:49 +0300

    header

---
+++ src/components/footer/Copyright.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Copyright extends Component {
+    render() {
+        return (
+            <>
+                Государственная Транспортная Лизинговая Компания 2020—<span>2021</span>
+            </>
+        );
+    }
+}
