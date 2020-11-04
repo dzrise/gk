8dfbedae6676bc487a68574fe7d63058f0dd9e03
Ваше Имя <you@example.com>
2020-11-04 21:32:41 +0300

    no3d

--- src/components/Bkad/page/AccordianItem.js
+++ src/components/Bkad/page/AccordianItem.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 export default class AccordianItem extends Component {
 
+
     render() {
+        let itemWidth  = '';
+        if(this.props.class === 'slide-one'){
+            if(this.props.isActive){
+                itemWidth = '53%';
+            } else {
+                itemWidth='5%';
+            }
+        } else {
+            if (this.props.isActive) {
+                itemWidth = '62%';
+            } else {
+                itemWidth = '14%';
+
+            }
+        }
         let className  = 'slide ' + this.props.class;
         if (this.props.isActive) {
             className  += ' active'
         }
         let styleItem = {
-            zIndex: this.props.zIndex
+            zIndex: this.props.zIndex,
+            width:  itemWidth,
         }
         let styleArrow = {
             position: 'absolute',
